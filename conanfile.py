f577b96c308226480647dbb39c983e6a499700bb
mohamedsayed18 <mohamed95.a.s@gmail.com>
2026-06-25 08:42:44 +0400

    Add conan 2

---
+++ conanfile.py
@@ -0,0 +1,27 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeDeps, CMakeToolchain
+
+
+class ChallengeProjectConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    requires = (
+        "boost/1.86.0",
+        "gtest/1.15.0",
+    )
+    default_options = {
+        "boost/*:shared": True,
+        "gtest/*:shared": True,
+    }
+
+    def layout(self):
+        self.folders.source = "."
+        self.folders.build = "build"
+        self.folders.generators = "build/generators"
+
+    def generate(self):
+        toolchain = CMakeToolchain(self)
+        toolchain.user_presets_path = False
+        toolchain.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
